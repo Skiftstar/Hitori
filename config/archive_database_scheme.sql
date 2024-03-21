c84e85c4ec38bcc10b325c47e189519d3df201bf
Skiftstar <yukamitft@gmail.com>
2024-03-21 17:45:19 +0100

    db scheme

---
+++ config/archive_database_scheme.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Channels (
+    ChannelID INTEGER PRIMARY KEY,
+    ChannelName TEXT NOT NULL
+);
+
+CREATE TABLE Threads (
+    ThreadID INTEGER PRIMARY KEY,
+    ChannelID INTEGER,
+    ThreadName TEXT NOT NULL,
+    FOREIGN KEY(ChannelID) REFERENCES Channels(ChannelID)
+);
+
+CREATE TABLE Users (
+    UserID INTEGER PRIMARY KEY,
+    UserName TEXT NOT NULL,
+    Discriminator INTEGER NOT NULL
+);
+
+CREATE TABLE Messages (
+    MessageID INTEGER PRIMARY KEY,
+    UserID INTEGER,
+    ThreadID INTEGER,
+    Content TEXT,
+    Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(UserID) REFERENCES Users(UserID),
+    FOREIGN KEY(ThreadID) REFERENCES Threads(ThreadID)
+);
+
+CREATE TABLE Media (
+    MediaID INTEGER PRIMARY KEY,
+    MessageID INTEGER,
+    URL TEXT,
+    Type TEXT,
+    FOREIGN KEY(MessageID) REFERENCES Messages(MessageID)
+);
