0691d5fdb55b3d2ba37746d2a80c2ccdd621813d
Skiftstar <yukamitft@gmail.com>
2024-03-26 09:51:56 +0100

    install chakra ui for website

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,12 +1,13 @@
-import React from "react"
-import logo from "./logo.svg"
+import { ChakraProvider } from "@chakra-ui/react"
 import "./App.css"
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
+    <ChakraProvider>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Simple React Typescript Tailwind Sample
+      </h1>
+    </ChakraProvider>
   )
 }
 
