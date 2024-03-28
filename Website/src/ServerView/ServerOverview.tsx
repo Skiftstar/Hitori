93146332872d00d0278c33945f2ba3dbea825817
Skiftstar <yukamitft@gmail.com>
2024-03-28 20:17:49 +0100

    Table Sorting

--- Website/src/ServerView/ServerOverview.tsx
+++ Website/src/ServerView/ServerOverview.tsx
@@ -5,30 +5,76 @@ import { ArchivedServer } from "./types"
 
 const ServerOverview = () => {
   const [entries, setEntries] = useState<ArchivedServer[]>([])
+  const [sortField, setSortField] = useState<string | null>(null)
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc" | "none">(
+    "none"
+  )
 
   useEffect(() => {
     const fetchEntries = async () => {
-      const data = await getArchivedServers() // Replace this with your function to fetch entries
+      const data = await getArchivedServers()
       setEntries(data)
     }
 
     fetchEntries()
   }, [])
 
-  const columns = ["ID", "Icon", "Name", "Created", "Archived", "Updated"]
+  useEffect(() => {
+    const sortedEntries = [...entries].sort((a: any, b: any) => {
+      if (sortDirection === "none" || sortField === null) {
+        // When unsorted, sort by 'id'
+        return a.id < b.id ? -1 : a.id > b.id ? 1 : 0
+      } else {
+        // When sorted, sort by the selected field
+        if (a[sortField] < b[sortField]) {
+          return sortDirection === "asc" ? -1 : 1
+        }
+        if (a[sortField] > b[sortField]) {
+          return sortDirection === "asc" ? 1 : -1
+        }
+        return 0
+      }
+    })
+    setEntries(sortedEntries)
+  }, [sortField, sortDirection])
+
+  const columns = ["id", "icon", "serverName", "created", "archived", "updated"]
+
+  const handleSort = (field: string) => {
+    if (sortField === field) {
+      setSortDirection(
+        sortDirection === "asc"
+          ? "desc"
+          : sortDirection === "desc"
+          ? "none"
+          : "asc"
+      )
+    } else {
+      setSortField(field)
+      setSortDirection("asc")
+    }
+  }
 
   return (
     <Table variant="simple">
       <Thead>
         <Tr>
           {columns.map((column, index) => (
-            <Th key={index}>{column}</Th>
+            <Th className="select-none cursor-pointer" key={index} onClick={() => handleSort(column)}>
+              {column}
+              {sortField === column &&
+                (sortDirection === "asc"
+                  ? "↑"
+                  : sortDirection === "desc"
+                  ? "↓"
+                  : "")}
+            </Th>
           ))}
         </Tr>
       </Thead>
       <Tbody className="text-text-color">
         {entries.map((entry, index) => (
-          <Tr key={index}>
+          <Tr key={index} className="hover:bg-secondary-color cursor-pointer">
             <Td>{entry.id}</Td>
             <Td></Td>
             <Td>{entry.serverName}</Td>
